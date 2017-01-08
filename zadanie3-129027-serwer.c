0069a8d7a89af3d94453b72c068bcf7892ff680a
Adrian Wawrzak <adiq94@gmail.com>
2017-01-08 23:04:45 +0100

    asd

--- zadanie3-129027-serwer.c
+++ zadanie3-129027-serwer.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int main() {
-    printf("Hello, World!\n");
+    char name[] = "myfifo";
+    int fd;
+    if (mkfifo(name, S_IRUSR | S_IWUSR) == -1) {
+        perror("fifo already exists");
+        //exit(1);
+    }
+    if ((fd = open(name, O_RDONLY)) == -1) {
+        perror("error on opening file");
+        exit(1);
+    }
+    while(1){
+
+    }
     return 0;
 }
